efa1878d2d608b95eefc9b16e9db9dd8ccfb7063
kinkenkan298 <akin@programmer.net>
2026-06-13 01:26:12 +0700

    refactor(plugin): pindahkan konfigurasi plugin ke namespace kien
    Memindahkan daftar plugin, konfigurasi Conform, dan Treesitter ke lua/kien agar struktur plugin lebih terpisah dari root lua.

--- lua/kien/pack.lua
+++ lua/kien/pack.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
 	"https://github.com/bluz71/vim-moonfly-colors",
-	"https://github.com/nvim-mini/mini.nvim",
+	-- "https://github.com/nvim-mini/mini.nvim",
 	"https://github.com/saghen/blink.lib",
 	"https://github.com/saghen/blink.cmp",
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },

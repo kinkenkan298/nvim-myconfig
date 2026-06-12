188816d61d79c0eb6bbbdaded7d2ac9ea7c62a51
kinkenkan298 <akin@programmer.net>
2026-06-13 03:54:09 +0700

    chore(pack): muat konfigurasi LSP dan Mason

---
+++ lua/kien/plugins/lsp/mason.lua
@@ -0,0 +1,37 @@
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local mason_tool_installer = require("mason-tool-installer")
+
+mason.setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+        },
+    },
+})
+
+mason_lspconfig.setup({
+    automatic_enable = false, -- set true to enable all LSP in `ensure_installed`
+    ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "html",
+        "cssls",
+        "gopls",
+        "tailwindcss",
+        "emmet_ls",
+        "emmet_language_server",
+    },
+})
+
+mason_tool_installer.setup({
+    ensure_installed = {
+        "prettier",
+        "stylua",
+        "pint",
+        "intelephense"
+    },
+
+})

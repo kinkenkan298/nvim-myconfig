a43b91e37c748cdf1fdbba5ba421ee0dd5e75e61
kinkenkan298 <akin@programmer.net>
2026-06-13 03:21:19 +0700

    feat(lsp): tambahkan konfigurasi lsp dan diagnostik

---
+++ lua/kien/plugins/lsp/lspconfig.lua
@@ -0,0 +1,75 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        -- Buffer local mappings
+        local opts = { buffer = ev.buf, silent = true }
+        -- Keymaps
+        opts.desc = "Show LSP references"
+        vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+        opts.desc = "Go to declaration"
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+        opts.desc = "Show LSP definitions"
+        vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+        opts.desc = "Show LSP implementations"
+        vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+        opts.desc = "Show LSP type definitions"
+        vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+        opts.desc = "See available code actions"
+        vim.keymap.set({ "n", "v" }, "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+
+        opts.desc = "Smart rename"
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+        opts.desc = "Show buffer diagnostics"
+        vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+        opts.desc = "Show line diagnostics"
+        vim.keymap.set("n", "df", vim.diagnostic.open_float, opts)
+
+        opts.desc = "Signature Help"
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+        opts.desc = "Show documentation for what is under cursor"
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+        opts.desc = "Restart LSP"
+        vim.keymap.set("n", "<leader>rs", ":lsp restart<CR>", opts)
+    end,
+})
+
+local signs = {
+    [vim.diagnostic.severity.ERROR] = " ",
+    [vim.diagnostic.severity.WARN] = " ",
+    [vim.diagnostic.severity.HINT] = "󰠠 ",
+    [vim.diagnostic.severity.INFO] = " ",
+}
+
+vim.diagnostic.config({
+    signs = { text = signs },
+    virtual_text = true,
+    underline = true,  -- Always on
+    update_in_insert = false,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = true,
+    },
+})
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local ok, blink = pcall(require, "blink.cmp")
+if ok then
+    capabilities = blink.get_lsp_capabilities(capabilities)
+end
+
+vim.lsp.config('*', {
+    capabilities = capabilities
+})

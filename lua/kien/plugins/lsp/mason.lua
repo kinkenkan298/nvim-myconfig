af1c2c712343bd59a6ac6a46430e4d003f468e42
kinkenkan298 <akin@programmer.net>
2026-06-13 04:33:50 +0700

    feat(mason): aktifkan otomatis lsp dan rapikan daftar tool

--- lua/kien/plugins/lsp/mason.lua
+++ lua/kien/plugins/lsp/mason.lua
@@ -3,35 +3,34 @@ local mason_lspconfig = require("mason-lspconfig")
 local mason_tool_installer = require("mason-tool-installer")
 
 mason.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-        },
-    },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 mason_lspconfig.setup({
-    automatic_enable = false, -- set true to enable all LSP in `ensure_installed`
-    ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-        "html",
-        "cssls",
-        "gopls",
-        "tailwindcss",
-        "emmet_ls",
-        "emmet_language_server",
-    },
+	automatic_enable = true, -- set true to enable all LSP in `ensure_installed`
+	ensure_installed = {
+		"lua_ls",
+		"ts_ls",
+		"html",
+		"cssls",
+		"gopls",
+		"tailwindcss",
+		"emmet_ls",
+		"emmet_language_server",
+	},
 })
 
 mason_tool_installer.setup({
-    ensure_installed = {
-        "prettier",
-        "stylua",
-        "pint",
-        "intelephense"
-    },
-
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"pint",
+		"intelephense",
+	},
 })

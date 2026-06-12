055328b71faee754b2f0d6c880b67c6500296247
kinkenkan298 <akin@programmer.net>
2026-06-13 04:33:50 +0700

    chore(formatter): nonaktifkan formatter python bawaan

--- lua/kien/plugins/conform-format.lua
+++ lua/kien/plugins/conform-format.lua
@@ -2,7 +2,7 @@ local config = {
 	formatters_by_ft = {
 		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
+		-- python = { "isort", "black" },
 		-- You can customize some of the format options for the filetype (:help conform.format)
 		rust = { "rustfmt", lsp_format = "fallback" },
 		-- Conform will run the first available formatter

a81779366da3299b9813be22f4c8261040473cdf
kinkenkan298 <akin@programmer.net>
2026-06-13 04:00:39 +0700

    feat(plugins): tambah konfigurasi autopairs dan blink cmp

---
+++ lua/kien/plugins/blink.lua
@@ -0,0 +1,54 @@
+require("blink.cmp").setup({
+    -- keymaps 
+    keymap = {
+        preset = "enter",
+    },
+
+    -- editor insert mode completions
+    completion = {
+        menu = {
+            auto_show = true, -- show on type
+        },
+        documentation = {
+            auto_show = true, -- show function signature/docs 
+        },
+        ghost_text = {
+            enabled = false,
+            show_with_menu = false,
+        },
+        accept = {
+            auto_brackets = {
+                enabled = true,
+            },
+        },
+    },
+
+    -- cmdline completions
+    cmdline = {
+        enabled = true,
+        keymap = { preset = "cmdline" },
+        completion = {
+            menu = { auto_show = true },
+        },
+    },
+
+    sources = {
+        default = { "lsp", "path", "buffer", "snippets" },
+        providers = {
+            lsp = {
+                opts = {
+                    tailwind_color_icon = "󱓻"
+                }
+            }
+        }
+    },
+
+    appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+    },
+
+    -- snippets = {
+    --     preset = "luasnip"
+    -- },
+})

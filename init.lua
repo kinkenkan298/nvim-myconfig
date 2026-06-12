25e52600e563ffa45843498030c8c4c634fbbf9a
kinkenkan298 <akin@programmer.net>
2026-06-12 14:10:09 +0700

    feat(init): tambahkan entrypoint konfigurasi neovim

---
+++ init.lua
@@ -0,0 +1,17 @@
+require("vim._core.ui2").enable({
+	enable = true,
+	msg = {
+		target = "cmd",
+		pager = { height = 0.5 },
+		dialog = { height = 0.5 },
+		cmd = { height = 0.5 },
+		msg = { height = 0.5, timeout = 4500 },
+	},
+})
+
+require("options")
+require("keymaps")
+require("commands")
+require("pack")
+
+vim.cmd.colorscheme("moonfly")

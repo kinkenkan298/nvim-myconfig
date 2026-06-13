ec3c5798b78cf4c34e6f9108b5d131b17635f74e
kinkenkan298 <akin@programmer.net>
2026-06-13 11:42:21 +0700

    feat(chadrc): tambah konfigurasi ui nvchad
    Tambahkan konfigurasi base46, statusline, telescope, dan tabufline untuk tema onedark.

---
+++ lua/chadrc.lua
@@ -0,0 +1,26 @@
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "onedark",
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+}
+
+M.ui = {
+	statusline = {
+		separator_style = "round",
+		theme = "default",
+		enabled = true,
+	},
+	telescope = {
+		style = "borderless",
+	},
+	tabufline = {
+		enabled = false,
+	},
+}
+
+return M

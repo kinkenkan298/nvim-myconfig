fb862633e9a8698684177a90ad719c57cd6c5903
kinkenkan298 <akin@programmer.net>
2026-06-14 21:05:00 +0700

    docs(conform): hapus komentar formatter yang redundan

--- lua/kien/plugins/conform-format.lua
+++ lua/kien/plugins/conform-format.lua
@@ -1,7 +1,6 @@
 local config = {
 	formatters_by_ft = {
 		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
 		-- python = { "isort", "black" },
 		-- You can customize some of the format options for the filetype (:help conform.format)
 		rust = { "rustfmt", lsp_format = "fallback" },

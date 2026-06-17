d8978094f183f28d6d7d3f764cb6123e0dd35ad8
kinkenkan298 <akin@programmer.net>
2026-06-17 16:05:43 +0700

    feat(laravel): enable blade-nav setup in Neovim config

--- lua/kien/plugins/laravel.lua
+++ lua/kien/plugins/laravel.lua
@@ -31,58 +31,58 @@ vim.keymap.set("n", "gf", function()
 	return "gf"
 end, { noremap = true, expr = true, desc = "Laravel:  Go To Resource" })
 
--- require("blade-nav").setup({
--- 	enable = true,
--- 	force_enable = false,
---
--- 	cache_timeout = 50000, -- Cache TTL in ms
--- 	debug = false,
---
--- 	-- Completion behavior
--- 	close_tag_on_complete = true,
--- 	include_routes_in_cmp = true,
---
--- 	-- Inertia
--- 	inertia_pages_path = nil, -- nil = "Pages" (default)
--- 	inertia_extensions = { "vue", "tsx", "jsx", "ts" },
---
--- 	-- Vue
--- 	jsconfig_path = "./jsconfig.json",
---
--- 	-- Extra directories for <x-component> resolution
--- 	laravel_components_paths = {
--- 		"resources/views/components",
--- 	},
---
--- 	-- Navigation targets (gf)
--- 	handlers = {
--- 		directive = true,
--- 		view = true,
--- 		livewire = true,
--- 		route = true,
--- 		config = true,
--- 		component = true,
--- 		inertia = true,
--- 		vue = true,
--- 	},
---
--- 	-- Completion/integration sources
--- 	integrations = {
--- 		gf = false,
--- 		cmp = false,
--- 		blink = true,
--- 		coq = false,
--- 		health = true,
--- 	},
---
--- 	-- Inline value annotations
--- 	annotations = {
--- 		show = false, -- Start with annotations visible
--- 		hl = "Comment", -- Highlight group for virtual text
--- 		prefix = " ⟶ ", -- Prefix before each value
--- 		max_len = 160, -- Max display length before truncation
--- 		debounce_ms = 120, -- Debounce for re-rendering on edits
--- 		show_on_load = true, -- Render annotations when a buffer loads
--- 		create_keymaps = true, -- Create K, <leader>bv, <leader>bcc maps
--- 	},
--- })
+require("blade-nav").setup({
+	enable = true,
+	force_enable = false,
+
+	cache_timeout = 50000, -- Cache TTL in ms
+	debug = false,
+
+	-- Completion behavior
+	close_tag_on_complete = true,
+	include_routes_in_cmp = true,
+
+	-- Inertia
+	inertia_pages_path = nil, -- nil = "Pages" (default)
+	inertia_extensions = { "vue", "tsx", "jsx", "ts" },
+
+	-- Vue
+	jsconfig_path = "./jsconfig.json",
+
+	-- Extra directories for <x-component> resolution
+	laravel_components_paths = {
+		"resources/views/components",
+	},
+
+	-- Navigation targets (gf)
+	handlers = {
+		directive = true,
+		view = true,
+		livewire = true,
+		route = true,
+		config = true,
+		component = true,
+		inertia = true,
+		vue = true,
+	},
+
+	-- Completion/integration sources
+	integrations = {
+		gf = false,
+		cmp = false,
+		blink = true,
+		coq = false,
+		health = true,
+	},
+
+	-- Inline value annotations
+	annotations = {
+		show = false, -- Start with annotations visible
+		hl = "Comment", -- Highlight group for virtual text
+		prefix = " ⟶ ", -- Prefix before each value
+		max_len = 160, -- Max display length before truncation
+		debounce_ms = 120, -- Debounce for re-rendering on edits
+		show_on_load = true, -- Render annotations when a buffer loads
+		create_keymaps = true, -- Create K, <leader>bv, <leader>bcc maps
+	},
+})
